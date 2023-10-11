fbf8495e9c05477471b5c16fcae36f1dc4e8b630
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-10-11 09:33:46 +0400

    js intro

---
+++ app.js
@@ -0,0 +1,59 @@
+// console.log("Hello world");
+// console.log("Salam Dunya");
+
+// a=6;
+
+// int a =6;
+
+// let, const, var
+
+// let value = 6;
+
+//! primitive and non-primitive(reference)
+
+//! primitive --> number, string, boolean, symbol, bigInt, undefined, null
+//! non-primitive --> Object (Array, Object, Date, Regex, Funtion)
+
+//! 5 9 81 76 int
+// 5.2 6.9 7.4 (float, double)
+
+// let name = "Hajar";
+// let age = 21;
+// let surname = "Abdullayeva";
+// let isMarried = false;
+// let isStudent = true;
+// let a = Symbol("a");
+
+// let newNumber;
+// let isTeacher = true;
+
+// let x = undefined;
+// let y = null;
+
+// console.log(typeof name);
+// console.log(name);
+// console.log(isMarried); //false
+// console.log(typeof isMarried);
+
+// console.log("Firuza", "Rugayya");
+// console.log(name, age, surname);
+// ("Name: Hajar, surname; A");
+
+// console.log("Name: ", name);
+// console.log("Surname: ", surname);
+
+// console.log("type of isTeacher: ", typeof isTeacher);
+
+// Template Literals
+
+// ("Hello, my name is Hajar. I am 22nd years old.");
+
+// console.log("Hello, my name is", name + ". I am", age, "years old.");
+
+// console.log(`Hello, my name is ${name}. I am ${age} years old.`);
+
+// let, const
+
+// const a = 5;
+// a = 80;
+// console.log(a);
