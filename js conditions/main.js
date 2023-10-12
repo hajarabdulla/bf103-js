8e9b8959c9147fefd0001117f5872fa3980df8d7
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-10-12 09:20:38 +0400

    js conditions

---
+++ js conditions/main.js
@@ -0,0 +1,134 @@
+// let num = 45;
+
+// // 45 -- this is 45
+
+// if (num == 45) {
+//   // code block
+//   console.log("Number equals to 45");
+// } else if (num > 3) {
+//   console.log("Greater than 3");
+// } else {
+//   console.log("Not bigger than 3");
+// }
+
+// let num = -1;
+// if (num > 0) {
+//   console.log("Positive");
+// } else if (num == 0) {
+//   console.log("Zero");
+// } else {
+//   console.log("Negative");
+// }
+
+//! 1 - 3 (initial)
+//! 4-7 (average)
+//! 8-11 (high)
+
+// let grade = 22;
+
+// if (grade >= 1 && grade <= 3) {
+//   console.log("Initial");
+// } else if (grade >= 4 && grade <= 7) {
+//   console.log("Average");
+// } else if (grade >= 8 && grade <= 11) {
+//   console.log("High");
+// } else {
+//   console.log("Please enter the valid number");
+// }
+
+//! cutdur, tekdir
+// let value = 22;
+// if (value % 2 == 0) {
+//   console.log("Even number");
+// } else {
+//   console.log("Odd number");
+// }
+
+//! her ikisi cutdurse --> 1; her ikisi tekdirse --> 1; biri tek biri cutdurse --> 0
+// let num1 = 36;
+// let num2 = 60;
+
+// if ((num1 % 2 == 0 && num2 % 2 == 0) || (num1 % 2 != 0 && num2 % 2 != 0)) {
+//   console.log(1);
+// } else {
+//   console.log(0);
+// }
+
+//!
+// let value = 12;
+// let a = 3;
+// let b = 4;
+
+// if (value % a == 0 && value % b == 0) {
+//   console.log("Yes");
+// } else if (value % a == 0 || value % b == 0) {
+//   console.log("Partial");
+// } else {
+//   console.log("No");
+// }
+
+// let dayOfWeek = 4;
+
+// if (dayOfWeek == 1) {
+//   console.log("Monday");
+// } else if (dayOfWeek == 2) {
+//   console.log("Tuesday");
+// } else if (dayOfWeek == 7) {
+//   console.log("Sunday");
+// } else {
+//   console.log("Invalid day of week");
+// }
+
+// switch (dayOfWeek) {
+//   case 1:
+//     console.log("Monday");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// let num = 6;
+
+// switch (true) {
+//   case num > 5:
+//     console.log("Bigger than 5");
+//     break;
+//   default:
+//     console.log("Not bigger");
+// }
+
+// let age = +prompt("Enter your number");
+
+// if (age >= 18) {
+//   document.write(age, "Can drive");
+//   console.log(age, "Can drive");
+// } else if (!age) {
+//   document.write("Canceled");
+//   console.log("Canceled");
+// } else {
+//   document.write("Cannot drive");
+//   console.log("Cannot drive");
+// }
+
+// let answer = age >= 18 ? "Can drive" : "Cannot drive";
+
+// console.log(answer);
+
