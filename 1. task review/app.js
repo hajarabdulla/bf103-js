322055a2563c0b484952fa80801baa801d069327
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-10-12 09:48:59 +0400

    js intro task review

---
+++ 1. task review/app.js
@@ -0,0 +1,23 @@
+//! Task 1
+// const num = 198;
+
+// let lastNumber = num % 10;
+// let firstNumber = parseInt(198 / 100);
+// console.log(firstNumber * 10 + lastNumber);
+
+//! Task2
+// let num = 512;
+
+// console.log(parseInt(512 / 10));
+
+//! Task 3
+
+let number = 1234;
+let lastNumber = number % 10; //4
+let thirdNumber = parseInt(number / 10) % 10; //3
+let secondNumber = parseInt(number / 100) % 10;
+let firstNumber = parseInt(number / 1000);
+
+let reversedNumber =
+  lastNumber * 1000 + thirdNumber * 100 + secondNumber * 10 + firstNumber;
+console.log(reversedNumber);
