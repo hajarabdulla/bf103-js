9b7e1d4644d8dcd9fa30383881c11b10c35c05dc
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-10-16 10:09:56 +0400

    array

---
+++ 4. Array BigO/app.js
@@ -0,0 +1,103 @@
+// Object (Array, Object, Function, Regex, Date)
+
+// let name = "Hajar";
+// const nameArr = [
+//   "Hajar",
+//   "Shahnaz",
+//   "Sabina",
+//   "Vafa",
+//   "Aytac",
+//   "Orkhan",
+//   "Rana",
+// ];
+// const numArr = [1, 2, 5, true, false, null, "Hajar", "Bdu"];
+
+// console.log(nameArr[2]);
+
+// console.log(numArr[4]);
+// console.log(nameArr.length);
+
+// console.log(numArr.length);
+// console.log(numArr[numArr.length - 1]);
+
+// console.log(nameArr.indexOf("Vafa"));
+
+// for (let i = 0; i <= nameArr.length - 1; i++) {
+//   console.log(i, nameArr[i]);
+// }
+
+//! push, pop, shift, unshift
+// const nameArr = [
+//   "Hajar",
+//   "Shahnaz",
+//   "Sabina",
+//   "Vafa",
+//   "Aytac",
+//   "Orkhan",
+//   "Rana",
+// ];
+
+// nameArr.push("Firuza", "Rugayya");
+// nameArr.pop();
+// nameArr.unshift("Firuza");
+// nameArr.shift();
+// console.log(nameArr);
+
+//! Task 1
+// const arr = [21, 45, 78, 3, 67, 4, 9];
+
+// let max = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log("max", max);
+
+//! Task 2
+// const arr = [21, 45, 78, 3, 67, 4, 9];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i] * 2);
+// }
+
+//! Task 3
+// const fruits = [
+//   "apple",
+//   "fig",
+//   "banana",
+//   "mango",
+//   "blueberry",
+//   "pomegranate",
+//   "guava",
+// ];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   if (fruits[i].length % 3 == 0) {
+//     console.log(fruits[i]);
+//   }
+// }
+
+//! Task 4
+// const arr = [21, 45, 78, 3, 67, 4, 9];
+
+// for (let i = 0; i < arr.length; i++){
+//     if(i % 2 == 0){
+//         console.log(arr[i]);
+//     }
+// }
+
+// let arr = [
+//   [2, 3],
+//   [4, 5],
+// ];
+
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     arr[i][j];
+//   }
+// }
+
+// let num = [3, 4, 9, 21];
+// const arr = [21, 45, 78, 3, 67, 4, 9];
