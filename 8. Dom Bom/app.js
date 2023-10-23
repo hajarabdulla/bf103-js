e1fa52ca46bc1e0d27175cba5f897c93a546c526
hajarabdullayeva <hadjar.abdullayeva@gmail.com>
2023-10-23 10:09:57 +0400

    dom

--- 8. Dom Bom/app.js
+++ 8. Dom Bom/app.js
@@ -25,10 +25,73 @@ console.log(window.location.protocol);
 //   document.write("HEllo");
 // }, 3000);
 
-const logger = setInterval(() => {
-  console.log("Hello world");
-}, 3000);
+// const logger = setInterval(() => {
+//   console.log("Hello world");
+// }, 3000);
 
-setTimeout(() => {
-  clearInterval(logger);
-}, 9500);
+// setTimeout(() => {
+//   clearInterval(logger);
+// }, 9500);
+
+console.log(document);
+
+// const title1 = document.getElementsByTagName("h1");
+// console.log(title1); //!Html Col
+// const title2 = document.getElementById("my-title");
+// console.log(title2);
+// const title3 = document.getElementsByClassName("class-title");
+// console.log(title3); //!Html Col
+
+// const title = document.querySelector("h1");
+// const title = document.querySelector("#my-title");
+// const title = document.querySelector(".class-title");
+// const title = document.querySelectorAll("h1"); //!Node List
+// console.log(title);
+
+// const title = document.querySelector("h1");
+// const body = document.querySelector("body");
+// const myInput = document.querySelector("input");
+// setTimeout(() => {
+//   title.style.background = "red";
+//   title.style.color = "yellow";
+//   title.style.textTransform = "uppercase";
+//   title.style.border = "1px solid gray";
+// }, 3000);
+
+// console.log(title.parentNode);
+// console.log(title.parentElement);
+
+// console.log(title.previousElementSibling);
+// console.log(title.nextElementSibling);
+
+// console.log(body.firstElementChild);
+// console.log(body.lastElementChild);
+// console.log(body.childElementCount);
+// console.log(body.children[0]); //! HTML Collection
+// console.log(body.childNodes);
+// console.log(title.innerHTML);
+
+// title.innerText = "BF-103";
+// title.innerHTML = "<span>Bf103</span>";
+// console.log(title.textContent);
+
+// setTimeout(() => {
+//   console.log(myInput.value);
+// }, 3000);
+
+// const formInput = document.querySelector(".form-element");
+
+// console.log(formInput.hasAttribute("type"));
+// formInput.setAttribute("type", "password");
+// console.log(formInput.getAttribute("type"));
+// console.log(formInput.getAttribute("class"));
+// formInput.removeAttribute("type")
+
+// const title = document.querySelector("h1");
+// title.classList.add("active");
+// title.classList.remove("class-title");
+// console.log(title.classList.contains("active"));
+// console.log(title.classList.contains("class-title"));
+// title.classList.replace("active", "notactive");
+
+
